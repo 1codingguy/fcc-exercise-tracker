1585b57a4b533d562b15b79e9e2ebb5d336413ac
coding-guy <1codingguy@gmail.com>
2022-08-27 16:33:05 +0100

    project init with connectDB() setup

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const connectDB = require('./db/connect')
 
 app.use(cors())
+
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
-});
-
-
-
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
 })
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+
+    app.listen(port, () => {
+      console.log(
+        `DB connection successful. Your app is listening on port ${port}`
+      )
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
